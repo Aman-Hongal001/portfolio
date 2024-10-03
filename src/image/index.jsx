ca19fd3f122877f4d0463881dc1ddcfe1f084abc
Aman <amanhongal@gmail.com>
2024-10-03 17:49:29 +0530

    Images required to use in site is added and imported

---
+++ src/image/index.jsx
@@ -0,0 +1,13 @@
+import fyImg from "./FinalYear.jpg";
+import iconImg from "./icon.jpg";
+import pmsImg from "./PMS.png";
+import profileImg from "./ProfileImg.png";
+import toDOImg from "./ToDo.jpg";
+
+export {
+    fyImg,
+    iconImg,
+    pmsImg,
+    profileImg,
+    toDOImg
+};
